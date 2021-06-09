871e34f4b3ff03f5e76259317b7f5b6a043634dc
Brissa <b.lopez24@alumnos.santotomas.cl>
2021-06-09 14:53:14 -0400

    Primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import {AddCategory} from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch']);
+
+    /* const handleAdd = () => {
+        //setCategories([...categories, 'HunterXHunter']);
+        setCategories( cats => [...categories, 'HunterXHunter']); //callback, devuelve el estado anterior en el primer argumentos, luego devuelve el nuevo estado
+        console.log(categories);
+        
+    }; */
+    console.log(categories);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr/>
+            {/* <button onClick={handleAdd}>Agregar</button> */}
+            <ol>
+                {
+                    categories.map( (category) => 
+                        <GifGrid
+                            category={ category}
+                            key={category}
+                        />
+                    )
+                }
+            </ol>
+        </>
+    )
+}
